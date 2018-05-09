15f86ca247febdd2faa1ce4e461b820b501d6e98
Vivial-DHarris <dharris@vivial.net>
2018-05-09 13:33:16 -0500

    Revised to working projects
    Not sure why it won't let me link to Core 2.0 Class libraries... Not
    worrying about it for now, online docs suggest it should be fine though.

--- Commons/Infrastructure/Interfaces/IContent.cs
+++ Commons/Infrastructure/Interfaces/IContent.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Drawing;
+﻿using System.Collections.Generic;
 
 namespace Common.Infrastructure.Interfaces
 {
     public interface IContent
     {
+        // TODO: For some reason it won't let me use .NET Core which contains this, and link it to the application??
+        //[StringLength(25)] 
         string Title { get; set; }
+
         string Description { get; set; }
         HashSet<string> Keywords { get; set; }
         string HeroImage { get; set; } 
--- NS2/Controllers/HomeController.cs
+++ NS2/Controllers/HomeController.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Services.Operators;
 
 namespace NS2.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index() {
-            return View();
+            LocalPostOperator postOperator = new LocalPostOperator();
+
+            return View(postOperator.Read("example1"));
         }
 
         public ActionResult About() {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
